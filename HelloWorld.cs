33974f0199f1b0367845deb9c2aba7690bc608ef
ZenCreator717 <IDenz16931@uat.edu>
2025-10-05 14:16:56 -0500

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Example: Print "Hello, World!" to the console
+        Debug.Log("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Example: Add functionality here if needed
+    }
+}
